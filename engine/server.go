e6a36b9aa42f08063e4797b5e41fba37028d2cc0
Geoff Hayes <hayesgm@gmail.com>
2013-08-29 17:50:29 -0700

    add assets change

--- engine/server.go
+++ engine/server.go
@@ -2,24 +2,20 @@ package engine
 
 import (
   "io/ioutil"
-  "runtime"
   "path"
   "fmt"
   "net/http"
   "html/template"
 )
 
-var _, filename, _, _ = runtime.Caller(0)
-var assetsDir = path.Join(path.Dir(filename), "../assets")
-
-func getHandler(filename string) func(w http.ResponseWriter, req *http.Request) {
+func getHandler(assetsDir, filename string) func(w http.ResponseWriter, req *http.Request) {
   return func(w http.ResponseWriter, req *http.Request) {
     tmpl := template.Must(template.ParseFiles(path.Join(assetsDir,filename)))
     tmpl.Execute(w, nil)
   }
 }
 
-func RegisterStaticAssets(root string) {
+func RegisterStaticAssets(assetsDir, root string) {
   // We're going to grab each asset from the assets directory and map it by name
   // Plus, we'll map a static route as home
   fmt.Println("Registering Static Assets...")
@@ -31,9 +27,9 @@ func RegisterStaticAssets(root string) {
     fmt.Printf("\tRegistering %v\n", file.Name())
     route := fmt.Sprintf("/%s", file.Name())
     
-    http.Handle(route, http.HandlerFunc(getHandler(file.Name())))
+    http.Handle(route, http.HandlerFunc(getHandler(assetsDir, file.Name())))
     if (file.Name() == root) {
-      http.Handle("/", http.HandlerFunc(getHandler(file.Name())))
+      http.Handle("/", http.HandlerFunc(getHandler(assetsDir, file.Name())))
       fmt.Printf("\tRegistered %v to /\n", file.Name())
     }
     fmt.Printf("\tRegistered %v to %v\n", file.Name(), route)
--- glime.go
+++ glime.go
@@ -5,13 +5,20 @@ import (
   "fmt"
   "log"
   "github.com/hayesgm/glime/engine"
+  "os"
+  "path"
 )
 
 func main() {
   fmt.Println("Running Glime Engine...")
-  engine.RegisterStaticAssets("glime.html")
+  wd, err := os.Getwd()
+  if (err != nil) {
+    log.Fatal("Unable to get cwd", err)
+  }
+  assetsDir := path.Join(wd, "assets")
+  engine.RegisterStaticAssets(assetsDir, "glime.html")
   engine.RegisterGameSocket()
-  err := http.ListenAndServe(":1111", nil)
+  err = http.ListenAndServe(":1111", nil)
   if err != nil {
     log.Fatal("Failed to launch server", err)
   }
